f2e34a54777b0febbfca750da1d6582a8fdd5fc1
Lance Good <lgoodyt@gmail.com>
2018-03-11 00:08:08 -0500

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,11 +1,4 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-data_file 'WEAPON_METADATA_FILE' 'weapons.meta'
-
-files {
-    'weapons.meta',
-    'explosionfx.dat'
-}
-
 client_script 'menu.lua'
 client_script 'client.lua'
